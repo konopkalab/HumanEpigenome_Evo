5542e0886ba5f088c05130b80379ade5e2949c53
emrecaglayan <47257634+emrecaglayan@users.noreply.github.com>
2023-10-09 12:55:25 -0500

    Update 01_PeakToGene.sh

--- MULTIOME_SCRIPTS/01_PeakToGene.sh
+++ MULTIOME_SCRIPTS/01_PeakToGene.sh
@@ -7,7 +7,7 @@ cd MULTIOME_FETAL
 for i in {1..178}
 do
 Rscript MULTIOME_FETAL/LinkPeaksToGenes.R 'MULTIOME_FETAL/seurObj.RDS' \
-	'/home2/s422159/workdir/pr5/MULTIOME_FETAL/pr_gns.RDS' \
+	'pr_gns.RDS' \
 	$i
 done
 
